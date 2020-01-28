26b4b39e71c399e89d1e8e21bd62654a47f269d6
osabl <andrii.osabliuk@gmail.com>
2020-01-28 03:32:10 +0200

    update default values

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const PASSWORD_LENGTH = 260;
-const DEFAULT_CHARS = {
-    upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    symbols: ['!', '@', '#', '$', '%', '^', '&', '*'],
-};
-const DEFAULT_DECOR = {
+const DEFAULT_TEMPLATES = {
+    char: {
+        upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+        lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        symbols: ['!', '@', '#', '$', '%', '^', '&', '*'],
+    },
     colors: ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple', 'gray'],
     figures: ['lineUP', 'triengleDOWN', 'lineLEFT', 'lineRIGHT'],
+    
 };
-
 const DEFAULT_REQUIREMENTS = {
-    upperCase: true,
-    lowerCase: true,
-    numbers: true,
-    symbols: true,
+    char: {
+        upperCase: true,
+        lowerCase: true,
+        numbers: true,
+        symbols: true,
+    },
     colors: true,
     figures: true,
 }
