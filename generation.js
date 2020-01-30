81841921186c097ba9a4b46e281810e89ff4c061
osabl <>
2020-01-30 15:08:08 +0200

    minor changes

--- generation.js
+++ generation.js
@@ -4,8 +4,8 @@ const PASSWORD_LENGTH = 260;
 const DEFAULT_CHUNK_LENGTH = 12;
 const DEFAULT_TEMPLATES = {
     char: {
-        upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-        lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+        lowercase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
         number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
         symbol: ['!', '@', '#', '$', '%', '^', '&', '*'],
     },
@@ -14,8 +14,8 @@ const DEFAULT_TEMPLATES = {
 };
 const DEFAULT_REQUIREMENTS = {
     char: {
-        upperCase: true,
-        lowerCase: true,
+        uppercase: true,
+        lowercase: true,
         number: true,
         symbol: true,
     },
